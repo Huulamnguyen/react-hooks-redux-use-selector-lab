69f249852b831787ed5611520253f12b38ee6788
huulamnguyen <liamnguyen.swe@gmail.com>
2022-03-18 18:37:40 -0400

    commit tree f835fc273b6a4caaab4d96004158af72b4893209

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from "./App";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(usersReducer);
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
